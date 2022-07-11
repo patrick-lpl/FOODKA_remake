5b58c5fa73dcad67b807fc9be99d7de208d4f38a
Lilyiro <916547796@qq.com>
2022-07-11 15:31:25 +0800

    实现图片上传功能，在index和menu添加链接，修改链接

--- src/main/java/com/swu/foodka/controller/PhotoController.java
+++ src/main/java/com/swu/foodka/controller/PhotoController.java
@@ -3,10 +3,7 @@ package com.swu.foodka.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -16,7 +13,7 @@ import java.util.UUID;
 /**
  * @author liuxiaolin
  */
-@Controller
+@RestController
 @CrossOrigin
 @RequestMapping("/img")
 public class PhotoController {
@@ -28,18 +25,21 @@ public class PhotoController {
     private String ProfilePhotoMapperPath; //图标映射路径
 
     @PostMapping("/api/profilePhotoUpload")
-    public String profilePhotoUpload(@RequestParam("files")MultipartFile fileUpload, Model model){
+    public String profilePhotoUpload(@RequestParam("file")MultipartFile fileUpload, Model model){
+        System.out.println("喂，有人吗？");
         // 获取文件名
         String fileName = fileUpload.getOriginalFilename();
+        System.out.println(fileName);
         // 获取文件后缀名，也可以在这里添加判断语句，规定特定格式到图片才能上传，否则拒绝保存
         String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        System.out.println(suffixName);
         // 为了避免文件发生图片替换，这里用了文件名重新生成
         fileName = UUID.randomUUID()+suffixName;
         try {
             // 将图片保存到文件夹里
             fileUpload.transferTo(new File(ProfilePhotoSavePath+fileName));
             model.addAttribute("thps",(ProfilePhotoMapperPath+fileName));
-            return "/photoReturn";
+            return fileName;
         } catch (Exception e) {
             e.printStackTrace();
         }
